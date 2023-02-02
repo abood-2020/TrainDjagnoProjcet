aa8915805b165010408b18f43eb6fa45b4332c51
Abd Al Razzaq Al Dahdouh <abdrazzaq20182019@gmail.com>
2023-02-02 18:54:05 +0200

    create model question

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+# CREATE TABLE Question name string , date datetime
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=12)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.question_text
